5e9f8f1a4760e475aea76dfa9938b15b274e84fd
Georgios11 <tsompanidis.georgios@gmail.com>
2025-03-22 12:35:58 +0100

    set up for user interactions done, ready for testing

---
+++ 01-rtl-tutorial/src/tutorial/04-user-interactions/Sandbox.test.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { render, screen, logRoles } from "@testing-library/react";
+import { test, expect, describe } from "vitest";
+import Sandbox from "./Sandbox";
