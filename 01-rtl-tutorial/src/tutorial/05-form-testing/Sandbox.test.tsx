2a52edfdb975682c34c3cf41aeeaeaf4bdcc7304
Georgios11 <tsompanidis.georgios@gmail.com>
2025-03-25 13:38:54 +0100

    remaining form tests done

--- 01-rtl-tutorial/src/tutorial/05-form-testing/Sandbox.test.tsx
+++ 01-rtl-tutorial/src/tutorial/05-form-testing/Sandbox.test.tsx
@@ -59,4 +59,67 @@ describe("05-form-testing", () => {
 		await user.type(confirmPasswordInputElement, "secret");
 		expect(confirmPasswordInputElement).toHaveValue("secret");
 	});
+	test("should show email error if email is invalid", async () => {
+		const { emailInputElement, submitButton } = getFormElements();
+		expect(screen.queryByText(/invalid email/i)).not.toBeInTheDocument();
+
+		await user.type(emailInputElement, "invalid");
+		await user.click(submitButton);
+		expect(screen.getByText(/invalid email/i)).toBeInTheDocument();
+	});
+	test("should password error if password is les than 5 characters", async () => {
+		const { emailInputElement, submitButton, passwordInputElement } =
+			getFormElements();
+		expect(
+			screen.queryByText(/password min length 5/i),
+		).not.toBeInTheDocument();
+
+		await user.type(emailInputElement, "test@test.email");
+		await user.type(passwordInputElement, "1111");
+		await user.click(submitButton);
+		expect(screen.getByText(/password min length 5/i)).toBeInTheDocument();
+	});
+	test("should show error if passwords do not match", async () => {
+		const {
+			emailInputElement,
+			submitButton,
+			passwordInputElement,
+			confirmPasswordInputElement,
+		} = getFormElements();
+		expect(
+			screen.queryByText(/password min length 5/i),
+		).not.toBeInTheDocument();
+
+		const errorMsg = /passwords do not match/i;
+		await user.type(emailInputElement, "test@test.email");
+		await user.type(passwordInputElement, "secret");
+		await user.type(confirmPasswordInputElement, "notsecret");
+		await user.click(submitButton);
+		expect(screen.queryByText(errorMsg)).toBeInTheDocument();
+	});
+	test("valid inputs show no errors and clear fields", async () => {
+		const {
+			emailInputElement,
+			submitButton,
+			passwordInputElement,
+			confirmPasswordInputElement,
+		} = getFormElements();
+
+		await user.type(emailInputElement, "test@test.email");
+		await user.type(passwordInputElement, "secret");
+		await user.type(confirmPasswordInputElement, "secret");
+		await user.click(submitButton);
+
+		expect(screen.queryByText(/invalid email/i)).not.toBeInTheDocument();
+		expect(
+			screen.queryByText(/password min length 5/i),
+		).not.toBeInTheDocument();
+		expect(
+			screen.queryByText(/passwords do not match/i),
+		).not.toBeInTheDocument();
+
+		expect(emailInputElement).toHaveValue("");
+		expect(passwordInputElement).toHaveValue("");
+		expect(confirmPasswordInputElement).toHaveValue("");
+	});
 });
