a1652b1b95161fe329562feac5a595bd0b2cfe9f
Georgios11 <tsompanidis.georgios@gmail.com>
2025-03-28 13:19:24 +0100

    setup done

--- my template/src/random.test.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { describe, test, expect } from "vitest";
-
-describe("basic math", () => {
-	test("1+1 equals 2", () => {
-		expect(1 + 1).toBe(2);
-	});
-});
