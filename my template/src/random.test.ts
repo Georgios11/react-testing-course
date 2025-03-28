caf38117913d2264fb264c11ae990977f8dc8666
Georgios11 <tsompanidis.georgios@gmail.com>
2025-03-28 12:52:37 +0100

    test runs

---
+++ my template/src/random.test.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { describe, test, expect } from "vitest";
+
+describe("basic math", () => {
+	test("1+1 equals 2", () => {
+		expect(1 + 1).toBe(2);
+	});
+});
