1738958e788706c6471fce833997e10eea599367
Georgios11 <tsompanidis.georgios@gmail.com>
2025-03-28 16:57:38 +0100

    First form component test done

---
+++ 03-tdd-focus-flow-project/01-focus-flow-starter/src/__tests__/Form.test.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { render, screen } from "@testing-library/react";
+import { describe, test, expect, vi } from "vitest";
+import userEvent, { type UserEvent } from "@testing-library/user-event";
+import Form from "../components/Form";
+
+const getFormElements = () => {
+	return {
+		titleInput: screen.getByRole("textbox", { name: /title/i }),
+		descriptionInput: screen.getByRole("textbox", { name: /description/i }),
+		categorySelect: screen.getByRole("combobox", { name: /category/i }),
+		submitButton: screen.getByRole("button", { name: /add task/i }),
+	};
+};
+describe("Form Component", () => {
+	let user: UserEvent;
+	const mockOnSubmit = vi.fn();
+	beforeEach(() => {
+		mockOnSubmit.mockClear();
+		user = userEvent.setup();
+		render(<Form onSubmit={mockOnSubmit} />);
+	});
+
+	test("renders form with empty fields initially", () => {
+		const { titleInput, descriptionInput, categorySelect } =
+			getFormElements();
+
+		expect(titleInput).toHaveValue("");
+	});
+});
--- 03-tdd-focus-flow-project/01-focus-flow-starter/src/components/Form.tsx
+++ 03-tdd-focus-flow-project/01-focus-flow-starter/src/components/Form.tsx
@@ -1,7 +1,78 @@
+import { useState } from "react";
 import { type ItemWithoutID, ItemCategory } from "../utils";
 
 const Form = ({ onSubmit }: { onSubmit: (item: ItemWithoutID) => void }) => {
-	return <div>Form</div>;
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const [category, setCategory] = useState<ItemCategory | "">("");
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	};
+
+	const labelStyles = "text-sm font-medium leading-none block mb-2";
+	const inputStyles = "flex h-10 w-full rounded-md border px-3 py-2 text-sm";
+	return (
+		<div className="max-w-xl">
+			<h2 className="text-xl font-semibold mb-2">Add New Task</h2>
+			<form onSubmit={handleSubmit} className="space-y-4">
+				{/*title input */}
+				<div>
+					<label htmlFor="title" className={labelStyles}>
+						Title
+					</label>
+					<input
+						type="text"
+						id="title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						required
+						className={inputStyles}
+					/>
+				</div>
+				{/*description input */}
+				<div>
+					<label htmlFor="description" className={labelStyles}>
+						Description
+					</label>
+					<input
+						type="text"
+						id="description"
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
+						required
+						className={inputStyles}
+					/>
+				</div>
+				{/*category input */}
+				<div>
+					<label htmlFor="category" className={labelStyles}>
+						Category
+					</label>
+					<select
+						id="category"
+						value={category}
+						onChange={(e) =>
+							setCategory(e.target.value as ItemCategory)
+						}
+						className={inputStyles}
+					>
+						<option value="">Select Category</option>
+						<option value="urgent">Urgent</option>
+						<option value="important">Important</option>
+						<option value="normal">Normal</option>
+						<option value="low">Low</option>
+					</select>
+				</div>
+				<button
+					type="submit"
+					className="rounded text-sm font-medium bg-blue-600 text-white hover:bg-blue-700 h-10 px-4 py-3 "
+				>
+					Add Task
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Form;
