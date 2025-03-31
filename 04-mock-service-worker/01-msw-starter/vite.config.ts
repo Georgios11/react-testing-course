c1ea7e7babceee894332013ca79ee26a453fa038
Georgios11 <tsompanidis.georgios@gmail.com>
2025-03-31 11:25:40 +0200

    db.json created

--- 04-mock-service-worker/01-msw-starter/vite.config.ts
+++ 04-mock-service-worker/01-msw-starter/vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/vitest.setup.ts',
-  },
+	plugins: [react()],
+	server: {
+		port: 5174,
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./src/vitest.setup.ts",
+	},
 });
